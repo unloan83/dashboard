c1bdb911ddb30a858b466826bd0af980ed3ef7c5
unloan83 <indra.siddhi1083@gmail.com>
2026-06-07 13:23:01 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    devIndicators: false,
+    turbopack: {
+        root: process.cwd(),
+    },
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static2.finnhub.io',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
+
+export default nextConfig;
